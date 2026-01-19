3e6ca7801c2126241d6471b340f68353ca8a1dd6
qsddxx <lishuoxian@qq.com>
2026-01-19 20:42:26 +0800

    Delete build directory

--- build/HdrHistogram_c/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liyixian/YCSB-ELESTIC/HdrHistogram_c/examples
-# Build directory: /home/liyixian/YCSB-ELESTIC/build/HdrHistogram_c/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
